59526669ba050ad28364b6c90d34bd081bb32107
poljak07 <acmeta4@gmail.com>
2024-08-08 22:58:05 +0200

    pass tests

--- routes/api.php
+++ routes/api.php
@@ -21,12 +21,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
     // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
-    Route::prefix('v1')->group(function() {
         // Keep in mind that prefix should be /api/v1.
         // Add ONE line to assign 5 resource routes to TaskController
         // Put one code line here below
-        Route::apiResource('tasks', TaskController::class);
-    });
+        Route::apiResource('v1/tasks', TaskController::class);
 
 
 });
